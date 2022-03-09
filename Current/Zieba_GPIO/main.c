9cdf7da6b1dd1b1e5a36b697ec338f35ccf0d2b6
PawelZieba01 <zpawel.zieba@gmail.com>
2022-03-09 11:37:02 +0100

    4.10 Add correct bit masks

--- Current/Zieba_GPIO/main.c
+++ Current/Zieba_GPIO/main.c
@@ -1,5 +1,13 @@
 #include <LPC21xx.H>
-#define LED_0_bm (1<<16)
+
+#define LED0_bm (1<<16)
+#define LED1_bm (1<<17)
+#define LED2_bm (1<<18)
+#define LED3_bm (1<<19)
+#define LED4_bm (1<<20)
+#define LED5_bm (1<<21)
+#define LED6_bm (1<<22)
+#define LED7_bm (1<<23)
 
 void Delay(unsigned int uiDelayMilliseconds) 															//ok 65 sekund chyba wystarczy - uint
 {
@@ -13,15 +21,15 @@ void Delay(unsigned int uiDelayMilliseconds) 															//ok 65 sekund chyba
 
 int main()
 {
-	IO1DIR = IODIR1 | LED_0_bm;
+	IO1DIR = IODIR1 | LED3_bm;
 	
 	
 	while(1)
 	{
-		IO1SET = IO1SET | LED_0_bm;
+		IO1SET = IO1SET | LED3_bm;
 		Delay(50);
-		IO1CLR = IO1CLR | LED_0_bm;
+		IO1CLR = IO1CLR | LED3_bm;
 		Delay(50);
 	}
 }
-//4.8
+//4.10
