6a29362cc115aa1c9cb4fc6e4edf080080265482
PawelZieba01 <zpawel.zieba@gmail.com>
2022-03-09 08:18:09 +0100

    4.4 Add Delay function

--- Current/Zieba_GPIO/main.c
+++ Current/Zieba_GPIO/main.c
@@ -1,13 +1,22 @@
-#include <LPC21xx.H>					// Dolaczenie pliku biblioteki odpowiadajacej za obsluge procesora LPC2129
+#include <LPC21xx.H>
+
+void Delay()
+{
+	unsigned int uiDelayCounter;
+	
+	for(uiDelayCounter = 0; uiDelayCounter < 1000; uiDelayCounter++){}
+}
 
 int main()
 {
-	IO1DIR = IODIR1 | (1<<16);	//Wpisanie '1' na 16 pozycje w rejestrze IO1DIR (input output port1 direction)	-	 (1<<16) to maska bitowa (1 przesuniete w lewo o 16 miejsc)  -  | bitowy OR
-	IO1SET = IO1SET | (1<<16);	//jak wyzej dla portu SET
-	IO1CLR = IO1CLR | (1<<16);	//jak wyzej dla portu CLEAR
+	IO1DIR = IODIR1 | (1<<16);
+	
 	
 	while(1)
 	{
-		
+		IO1SET = IO1SET | (1<<16);
+		Delay();
+		IO1CLR = IO1CLR | (1<<16);
+		Delay();
 	}
 }
