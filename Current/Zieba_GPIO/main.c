d175ed485f61d3405fde93e2d4fc4ac3809e757c
PawelZieba01 <zpawel.zieba@gmail.com>
2022-03-16 10:31:02 +0100

    4.16 Add ReadButton1 function

--- Current/Zieba_GPIO/main.c
+++ Current/Zieba_GPIO/main.c
@@ -4,10 +4,11 @@
 #define LED1_bm (1<<17)
 #define LED2_bm (1<<18)
 #define LED3_bm (1<<19)
-#define LED4_bm (1<<20)
-#define LED5_bm (1<<21)
-#define LED6_bm (1<<22)
-#define LED7_bm (1<<23)
+#define BUTTON0_bm (1<<4)
+#define BUTTON1_bm (1<<6)
+#define BUTTON2_bm (1<<5)
+#define BUTTON3_bm (1<<7)
+
 
 
 
@@ -17,7 +18,7 @@ void Delay(unsigned int uiDelayMilliseconds) 															//ok 65 sekund chyba
 	
 	for(; uiDelayMilliseconds > 0; uiDelayMilliseconds--)										//dwie petle bo mnozenie troche zajmuje
 	{
-			for(uiDelayCounter = 0; uiDelayCounter < 7500; uiDelayCounter++){}
+		for(uiDelayCounter = 0; uiDelayCounter < 7500; uiDelayCounter++){}
 	}
 }
 
@@ -32,33 +33,49 @@ void LedOn(unsigned char ucLedIndeks)
 	switch(ucLedIndeks)
 	{
 		case 0:
-			IO1SET |= LED0_bm;
-			break;
+            IO1SET |= LED0_bm;
+            break;
 		case 1:
-			IO1SET |= LED1_bm;
-			break;
+            IO1SET |= LED1_bm;
+            break;
 		case 2:
-			IO1SET |= LED2_bm;
-			break;
+            IO1SET |= LED2_bm;
+            break;
 		case 3:
-			IO1SET |= LED3_bm;
-			break;
+            IO1SET |= LED3_bm;
+            break;
+	}
+}
+
+unsigned char ReadButton1()
+{
+    if((IO0PIN & BUTTON0_bm) == 0)
+    {
+        return 1;
+    }
+    else    
+    {
+        return 0;
 	}
 }
 
 
 int main()
-{
-	unsigned char ucLedNumber = 0;
-	
+{	
 	LedInit();
 	
 	while(1)
 	{
-		LedOn(ucLedNumber % 4);
-		Delay(250);
-		IO1CLR |= (1<<(16 + (ucLedNumber % 4)));
-		ucLedNumber++;																													//po przekroczeniu zakresu ucLedNumber jest ok
+		if(ReadButton1() == 1)	
+		{
+			IO1SET |= LED1_bm;
+            IO1CLR |= LED0_bm;
+		}
+		else
+		{
+			IO1SET |= LED0_bm;
+            IO1CLR |= LED1_bm;
+		}
 	}
 }
-//4.14
+//4.16
