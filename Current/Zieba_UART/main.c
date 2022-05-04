7280c836d993a7caa734393e73e59b8e82dc2019
PawelZieba01 <zpawel.zieba@gmail.com>
2022-05-04 06:39:57 +0200

    10.3.4 UART with servo

--- Current/Zieba_UART/main.c
+++ Current/Zieba_UART/main.c
@@ -3,39 +3,43 @@
 #include "uart.h"
 #include "led.h"
 
+#define NULL '\0'
+
+void Delay(unsigned int uiDelayMilliseconds);
 
 /*******************************************/
 int main()
 {
+	unsigned int uiServoPosition = 0;
+	
 	UART_InitWithInt(9600);
-	LedInit();
-	KeyboardInit();
+	//KeyboardInit();
 	ServoInit(50);
 
 	while(1)
 	{
+		//Delay(100);
+		
 		switch(cOdebranyZnak)
 		{
 			case '1':
-				ServoGoTo(12);
-				break;
-			
-			case '2':
-				ServoGoTo(24);
-				break;
-			
-			case '3':
-				ServoGoTo(36);
+				uiServoPosition = uiServoPosition + 12;
+				ServoGoTo(uiServoPosition);
+				cOdebranyZnak = NULL;
 				break;
 			
 			case 'c':
 				ServoCallib();
+				uiServoPosition = 0;
+				cOdebranyZnak = NULL;	//albo tutaj trzeba wpisac nulla albo u gory odkomentowac delaya
 			
 			default:
 				break;
 		}
 		
-		switch(eKeyboardRead())
+		
+		
+		/*switch(eKeyboardRead())
 		{
 			case BUTTON_0:
 				ServoCallib();
@@ -55,32 +59,18 @@ int main()
 	
 			default:
 				break;
-		}  
-		
-		/*switch(cOdebranyZnak)
-		{
-			case '1':
-				LedOn(0);
-				break;
-			
-			case '2':
-				LedOn(1);
-				break;
-			
-			case '3':
-				LedOn(2);
-				break;
-			
-			case '4':
-				LedOn(3);
-				break;
-			
-			case 'c':
-				LedOn(4);
-			
-			default:
-				break;
-		}*/                 
+		}  */              
 	}
 }
 /*******************************************/
+
+
+void Delay(unsigned int uiDelayMilliseconds)
+{
+	unsigned int uiDelayCounter;
+	
+	for(; uiDelayMilliseconds > 0; uiDelayMilliseconds--)
+	{
+		for(uiDelayCounter = 0; uiDelayCounter < 7500; uiDelayCounter++){}
+	}
+}
