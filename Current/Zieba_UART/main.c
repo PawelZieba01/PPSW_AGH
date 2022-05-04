582fd82a04d5ece615903dd41f4fb8386e49a325
PawelZieba01 <zpawel.zieba@gmail.com>
2022-05-04 05:28:12 +0200

    10.3.3 UART with servo

--- Current/Zieba_UART/main.c
+++ Current/Zieba_UART/main.c
@@ -17,6 +17,49 @@ int main()
 		switch(cOdebranyZnak)
 		{
 			case '1':
+				ServoGoTo(12);
+				break;
+			
+			case '2':
+				ServoGoTo(24);
+				break;
+			
+			case '3':
+				ServoGoTo(36);
+				break;
+			
+			case 'c':
+				ServoCallib();
+			
+			default:
+				break;
+		}
+		
+		switch(eKeyboardRead())
+		{
+			case BUTTON_0:
+				ServoCallib();
+				break;
+			
+			case BUTTON_1:
+				ServoGoTo(12);
+				break;
+			
+			case BUTTON_2:
+				ServoGoTo(24);
+				break;
+			
+			case BUTTON_3:
+				ServoGoTo(36);
+				break;
+	
+			default:
+				break;
+		}  
+		
+		/*switch(cOdebranyZnak)
+		{
+			case '1':
 				LedOn(0);
 				break;
 			
@@ -37,30 +80,7 @@ int main()
 			
 			default:
 				break;
-		}
-		
-/*		switch(eKeyboardRead())
-		{
-			case BUTTON_0:
-				ServoCallib();
-				break;
-			
-			case BUTTON_1:
-				ServoGoTo(12);
-				break;
-			
-			case BUTTON_2:
-				ServoGoTo(24);
-				break;
-			
-			case BUTTON_3:
-				ServoGoTo(36);
-				break;
-	
-			default:
-				break;
-		}           */           
-
+		}*/                 
 	}
 }
 /*******************************************/
